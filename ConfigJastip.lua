9685f29de837bc3dbfd786677d84f464b153d4d2
notFlex <159158160+Alfnnnnyy@users.noreply.github.com>
2024-04-20 23:38:09 -0700

    Update and rename Jastip to ConfigJastip.lua

---
+++ ConfigJastip.lua
@@ -0,0 +1,104 @@
+---===== INFO BUYER =====--- 
+InfoLoop = true
+targetinfo = 1 --MENGIRIM WEBHOOK SETIAP BERAPA KALI LOOP?
+RingkasWorldName = false --Jadiin False Jika Gamau Nama Worldnya di Ringkas // INI COCOK UNTUK WORLDNYA KEBANYAKAN + KEPANJANGAN AGAR GA BUG WEBHOOK
+
+--==== USEFULL ====--
+PutJammer = true
+SetLevelWorld = 124
+
+---===== NEW V2 =====---
+MoveRange = 6
+MoveInterval = 200
+JedaHW = 7
+HW_Exit = true
+LanjutPlant = false -- APABILA true MAKA BOTNYA AUTO PLANT KE LISTPLANT | BOT TIDAK AKAN DROP SEED KE STORAGE
+
+--==== NEW V1.7 ====--
+PNBOtherWorld = true --APABILA INGIN PNB DILUAR FARM
+DurasiWorldPNB = 85 --BERAPA MENIT AGAR BOTNYA GANTI WORLD
+NewUpdate = true -- BOT BAKAL LEVELING SEBELUM PNB UNTUK COUNTER NEW PATCH.
+Leveling = true -- BOT AKAN LEVELING MENGGUNAKAN WINDOW, APABILA FALSE BOT AKAN LEVELING DENGAN CARA HARVEST FARM UTAMA!
+RarityLevel = 1 --Botnya bakal Leveling Hingga RarityLevel
+WorldWL = {"B2FE202842204191"} -- SEDIAKAN WL UNTUK BOT MEMBUAT WORLD PNB / LEVELING WINDOW
+WorldWindow= {"B2FE202842204191"} -- SEDIAKAN WINDOW SEED FLOATING UNTUK BOT MULAI LEVELING
+BersihkanWindow = true --BOTNYA KELARIN URUSAN SAMA WINDOW DULU SEBELUM LANJUT KE FARMABLE UTAMA
+MinBW = 90000 --BOT AKAN OFF KETIKA PLAYER YANG OFF SEBANYAK MinBW
+DurasiOff = 10 --BOT AKAN OFF SELAMA (DurasiOff * Semenit)
+
+---===== Bot Off =====---
+Istirahat = false --JADIIN FALSE JIKA GAMAU ISTIRAHATIN BOTNYA
+UTC_Time = 9 -- WIB 7 / WITA 8 / WIT 9
+JamTidur = {
+    "13:20",
+    "14:25",
+    "01:40"
+} -- JAM DAN MENIT BOTNYA OFF (Format 24 Jam)
+DelayTidur = 20 -- BOTNYA OFF SELAMA n DelayTidur (Format Menit)
+
+---===== New Variable on v4.8 =====---
+TilePNB = 5 --BOTNYA AKAN PNB 3 TILES!! PILIH 1~5
+BersihkanBlock = true --BOTNYA AKAN CLEAR PLACED BLOCK SEBELUM NEXT WORLD
+BersihkanFloat = true --BOTNYA AKAN BERSIHKAN FLOATING BLOCK SEBELUM NEXT WORLD
+SisaTree = 1 --BOTNYA AKAN NEXT WORLD APABILA SISA POHON READY BERJUMLAH <= SISATREE
+
+---===== BASIC SETTINGS =====---
+target_block = 200 -- TARGET MAX HARVEST (JML BLOCK DI TAS)
+target_seed = 5 -- TARGET MINIMAL SEED UNTUK DITANAM
+DelayRecon = 300000 --JEDA RECONNECT (Ms)
+delayht = 200 -- DELAY HARVEST
+delayplant = 220 -- DELAY PLANT
+delaypnb = 200 -- DELAY PNB
+delayplace = 190 -- DELAY PLACE
+delayworld = 10000 -- DELAY MASUK DOOR
+delaydrop = 1500 -- JEDA DROP PER ITEM
+delaytrash = 1000
+targettrash = 100 -- AUTO TRASH JUNK ITEMS KETIKA MENCUKUPI TARGET
+Turu = true --JADIIN FALSE JIKA GAMAU UNLIMITED WORLD
+
+---==== PACK SETTINGS ====---
+autobuypack = true
+hargapack = 45000 -- HARGA PACK
+namapack = "surg_value_pack" -- NAMA PACK
+MinItemPack = 100 --APABILA JUMLAH ITEM YANG ADA DIDALAM 
+maxbuy = 2 --MAKSIMAL BOTNYA AKAN REPEAT BELI PACK JIKA 
+iditempack = {1258, 1260, 1262, 1264, 1266, 1268, 1270, 4296, 4308, 4310, 4312, 4314, 4316, 4318, 9310}
+ID_AcuanDrop_Pack = 20 --BOT AKAN DROP PACK DI ACUAN Dirt Tree (3)d
+
+TargetLevel = 126 -- OTOMATIS GANTI AKUN APABILA MENCAPAI TARGET LEVEL
+
+---===== COCOK UNTUK EVENT! =====---
+SpecialItem = {5746, 5742, 5748} --ITEM YANG INGIN DIAMANKAN KE WORLD, COCOK UNTUK HARVEST FEST!
+minSpec = 200 --BOT AKAN DROP ITEM APABILA ITEM TSB MELEBIHI TARGET minSpec
+ID_AcuanDrop_Spec = 880 --BOT AKAN DROP SPECIAL ITEM DI DIRT TREE (3)
+
+---===== Others =====---
+DontTrash = {13160} --BOT GAK AKAN TRASH ITEM YANG DIBERIKAN
+AbaikanTile = {} --BOT GA AKAN PLANT DIATAS IDITEM YANG DIISIKAN
+
+--=== NEW VAR ===--
+IntervalPlus = 0 -- JEDA TIAP CALL FUNCTION
+ControlViaDC = true
+TimeRelogBotMatung = 15 --JIKA TIDAK ADA AKTIVITAS SELAMA 7 MENIT, AUTO RELOG
+HideWebhook = false --BOT GAK BAKAL NGIRIM WEBHOOK (CONDITIONAL VARIABLE LANJUTAN)
+ShowPingHook = true --bakal tetap ngasih info untuk PingHook
+ShowSeedInfo = true --bakal tetap ngasih info untuk Seed Hook
+ShowPackInfo = true --bakal tetap ngasih info untuk Pack Hook
+SimplePinghook = false
+
+---===== JOIN RANDOM WORLD =====---
+nextrandom = false --UBAH JADI FALSE JIKA GAMAU JOIN RANDOM WORLD SEBELUM NEXT WORLD
+storagerandom = false --UBAH JADI FALSE JIKA GAMAU JOIN RANDOM WORLD SETELAH BALIK DARI STORAGE
+randomworld = {"AWFIOJIOGFWKJFJEWFUIDI", "SFCSOEFCIOSAFJESIAISHUDA", "AWFCJEUIGRAKWEDKFHAWEH", "IOHSAICFHXQIEFIWJDKW", "OSICFHIFWWKHEWOEFWH"} --BEBAS MAU BERAPA WORLD BOTNYA JOIN RANDOM
+delayrandomworld = 35000
+
+---===== EMOJI =====---
+emot_bot = "<:Bothutao:1208255530183688242>"
+emot_gems = "<:gemssss:1208259674223288330>"
+emot_pack = "<:huu:1208257779471618068>"
+emot_world = "<:Huauuu:1208252935494963303>"
+emot_pickaxe = "<:mauuu:1208260046878675055>"
+emot_wl = "<:WORLDLOCK:1159565273947447346>"
+emot_rank = "<a:kurukuru_seseren:1105870509985509508>"
+emot_tas = "<:Emmyy:1208257800518500413>"
+emot_computer = "<:cpu:994228771014070372>"
